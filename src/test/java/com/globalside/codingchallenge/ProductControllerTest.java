91c2b5be2218eea884d7ed7d4c199dc6510d2a2f
Omar Khaled Hassan Elbeltagui <go29kut@mytum.de>
2026-08-08 20:04:05 +0200

    test user get /products

--- src/test/java/com/globalside/codingchallenge/ProductControllerTest.java
+++ src/test/java/com/globalside/codingchallenge/ProductControllerTest.java
@@ -1,9 +1,29 @@
-package com.globalside.codingchallenge.rbac.web;
+package com.globalside.codingchallenge.rbac;
 
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerTest {
+    @Autowired
     private MockMvc mvc;
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void requestProductsUrlWithUser() throws Exception {
+        mvc
+                .perform(get("/products"))
+                .andExpect(status().isOk());
+    }
+
+
 }
