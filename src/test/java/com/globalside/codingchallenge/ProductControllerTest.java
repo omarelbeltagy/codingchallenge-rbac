a6a209c76ac2499548d270c1abe303b5ad1222ad
Omar Khaled Hassan Elbeltagui <go29kut@mytum.de>
2026-08-08 19:46:55 +0200

    define mock mvc

--- src/test/java/com/globalside/codingchallenge/ProductControllerTest.java
+++ src/test/java/com/globalside/codingchallenge/ProductControllerTest.java
@@ -1,5 +1,9 @@
 package com.globalside.codingchallenge.rbac.web;
 
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+class ProductControllerTest {
+    private MockMvc mvc;
+}
