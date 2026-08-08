ab6cf66144355ae05aa7447a38de5e288ae469b0
Omar Khaled Hassan Elbeltagui <go29kut@mytum.de>
2026-08-08 19:01:36 +0200

    disable csrf, its hiding responses from my curl

--- src/main/java/com/globalside/codingchallenge/config/SecurityConfig.java
+++ src/main/java/com/globalside/codingchallenge/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+            .csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests((authorize) -> authorize
                 .requestMatchers(HttpMethod.GET, "/products/**").hasAnyRole("USER", "ADMIN")
                 .requestMatchers(HttpMethod.POST, "/products").hasAnyRole("ADMIN")
